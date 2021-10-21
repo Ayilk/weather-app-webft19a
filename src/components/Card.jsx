17ebbf51915eda1f2516e0bb54574de3fff079d4
Ayilk <srangel.h92@gmail.com>
2021-10-20 19:49:46 -0500

    create component card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({max, min, img,name,onClose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  function handleOnClose(){//Con esto me aseguro que el valor que voy a recibir en el componente
+    //onClose es una función, si no es una función no hace nada
+    if(typeof onClose==="function") onClose();
+  }
+  return (<div>
+    <button onClick={handleOnClose}>X</button>//Tenemos que pasar una función, no ejecutarla
+    <span>{name}</span>//Entre llaves porque estoy poniendo código js
+    <div>
+      <label>Min</label>
+      <span>{min}</span>// El valor me viene por propiedad
+    </div>
+    <div>
+      <label>Max</label>
+      <span>{max}</span>
+    </div>
+    <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="Imagen del clima"/>
+  </div>)
 };
